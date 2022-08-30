21db4b04a5fbdf5e46293a1a2d8ef48794e6eec1
Thiago Saytson <thiagosaytson@poli.ufrj.br>
2022-08-30 04:50:26 -0300

    botao enter funcionando

--- script.js
+++ script.js
@@ -1,67 +1,83 @@
 const userName = {
-    name: name
-}
-
+    name
+};
+document.addEventListener("keypress", (tecla) => {
+    if (tecla.key === "Enter") {
+        const button = document.querySelector(".bottom-bar ion-icon");
+        button.click();
+    }
+});
 function registerUser() {
-    userName.name = prompt('Digite seu nome: ');
-    const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', userName);
-    console.log(userName);
-    
-    console.log(requisicao);
+    userName.name = document.querySelector(".nome").value;
+    if (userName.name !== '') {
+        const requisicao = axios.post(
+            "https://mock-api.driven.com.br/api/v6/uol/participants",
+            userName
+        );
 
-    requisicao.then(getMessages);
-    requisicao.catch(registerUser);
+        console.log(requisicao);
+
+        requisicao.then(enterChat);
+        requisicao.catch(registerUser);
+    }
 }
-
+function enterChat() {
+    setInterval(keepAlive, 5000);
+    setInterval(getMessages, 3000);
+    const mensagens = document.querySelector(".messages");
+    mensagens.classList.remove("hidden");
+    const topbar = document.querySelector(".top-bar");
+    topbar.classList.remove("hidden");
+    const bottombar = document.querySelector(".bottom-bar");
+    bottombar.classList.remove("hidden");
+    const home = document.querySelector(".home");
+    home.classList.add("hidden");
+}
 function getMessages(resposta) {
     let message = {};
-    const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    const promessa = axios.get(
+        "https://mock-api.driven.com.br/api/v6/uol/messages"
+    );
+    const mensagens = document.querySelector(".messages");
     promessa.then((promessa) => {
-        const mensagens = document.querySelector(".messages");
-        console.log(promessa);
+        mensagens.innerHTML = '';
         for (let i = 0; i < promessa.data.length; i++) {
             message = promessa.data[i];
-            if (message.type === 'private-message' && message.to === userName.name) {
-                mensagens.innerHTML +=
-                    `<div class=${message.type}>
+            mensagens.innerHTML +=
+                `<div class=${message.type}>
                     <div class="time">
                         (${message.time})
                     </div>
                     <a>${message.from}</a> para <a>${message.to}:</a>
-                    ${message.text}`
-            } else if (message.type !== 'private-message') {
-                mensagens.innerHTML +=
-                    `<div class=${message.type}>
-                    <div class="time">
-                        (${message.time})
-                    </div>
-                    <a>${message.from}</a> para <a>${message.to}:</a>
-                    ${message.text}`
-            }
-
+                    ${message.text}`;
         }
-        mensagens.scrollIntoView({block:"end", inline:"end", behavior:"smooth"});
+        mensagens.scrollIntoView({
+            block: "end",
+            inline: "end",
+            behavior: "smooth"
+        });
     });
+
 }
 
-function keepAlive(userName) {
-    axios.post('https://mock-api.driven.com.br/api/v6/uol/status', userName);
+function keepAlive() {
+    axios.post("https://mock-api.driven.com.br/api/v6/uol/status", userName);
 }
 
 function sendMessage() {
-    elementoTexto = document.querySelector("input");
-    console.log('botão funciona');
+    const elementoTexto = document.querySelector(".bottom-bar input");
+    console.log("botão funciona");
+    console.log(elementoTexto.value);
     const message = {
         from: userName.name,
         to: 'Todos',
         text: elementoTexto.value,
         type: 'message'
-    }
-    const resposta = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', message);
+    };
+    const resposta = axios.post(
+        "https://mock-api.driven.com.br/api/v6/uol/messages",
+        message
+    );
     resposta.then(getMessages);
     resposta.catch(window.location.reload);
 }
-
-//registerUser();
-//setInterval(stillConnected, 5000);
-//setInterval(getMessages, 3000);
